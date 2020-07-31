5b924f911f2593894ef64c543abf3d8db2e3299d
Benjamin Klemencic <benjamin.klemencic@fau.de>
2020-07-31 12:38:24 +0200

    1.1.0 (taxonomy added to addMetadata())

--- rrze-glossary.php
+++ rrze-glossary.php
@@ -4,7 +4,7 @@
 Plugin Name:     RRZE Glossary
 Plugin URI:      https://gitlab.rrze.fau.de/rrze-webteam/rrze-glossary
 Description:     Plugin, um Glossar-Einträge zu erstellen, aus dem FAU-Netzwerk zu synchronisieren und mittels Shortcode oder als Block (Gutenberg Editor) einzubinden.  
-Version:         1.0.9
+Version:         1.1.0
 Author:          RRZE Webteam
 Author URI:      https://blogs.fau.de/webworking/
 License:         GNU General Public License v3
@@ -97,24 +97,41 @@ function system_requirements()
 }
 
 function addMetadata(){
-    $postIds = get_posts(
-        ['post_type' => 'glossary', 
-        'nopaging' => true, 
-        'fields' => 'ids'
-        ]
-    );
+    if (post_type_exists('glossary')){
+        // add metadata for posts with CPT glossary
+        $postIds = get_posts([
+            'post_type' => 'glossary', 
+            'nopaging' => true, 
+            'fields' => 'ids'
+            ]);
 
-    $lang = substr( get_locale(), 0, 2);
+        $lang = substr( get_locale(), 0, 2);
 
-    foreach( $postIds as $postID ){
-        if (metadata_exists('post', $postID, 'source') === false){
-            update_post_meta( $postID, 'source', 'website' );        
-            update_post_meta( $postID, 'remoteID', $postID );
-            update_post_meta( $postID, 'lang', $lang );
-            $remoteChanged = get_post_timestamp( $postID, 'modified' );
-            update_post_meta( $postID, 'remoteChanged', $remoteChanged );
+        foreach( $postIds as $postID ){
+            if (metadata_exists('post', $postID, 'source') === false){
+                update_post_meta($postID, 'source', 'website');        
+                update_post_meta($postID, 'remoteID', $postID);
+                update_post_meta($postID, 'lang', $lang);
+                $remoteChanged = get_post_timestamp( $postID, 'modified' );
+                update_post_meta( $postID, 'remoteChanged', $remoteChanged );
+            }
+        }    
+
+        if (taxonomy_exists('glossary_category')){
+            // add metadata for glossary_category including their children
+            $terms = get_terms([
+                'taxonomy' => 'glossary_category',
+                'hide_empty' => true
+                ]);
+
+            foreach( $terms as $term ){
+                if (metadata_exists('term', $term->term_id, 'source') === false){
+                    update_term_meta($term->term_id, 'source', 'website');        
+                    update_term_meta($term->term_id, 'lang', $lang);        
+                }
+            }    
         }
-    }    
+    }
 }
 
 /**
@@ -134,7 +151,7 @@ function activation() {
     // Ab hier können die Funktionen hinzugefügt werden,
     // die bei der Aktivierung des Plugins aufgerufen werden müssen.
     // Bspw. wp_schedule_event, flush_rewrite_rules, etc.
-    // Einmaliger Aufruf: vom alten Plugin oder Fallback vom Theme gespeicherte Daten zum CPT "glossary" um Metadaten ergaenzen, damit rrze-glossary (version >= 2.0) funktioniert:
+    // Einmaliger Aufruf: vom Theme gespeicherte Daten zum CPT "glossary" und zur taxonomy "glossary_category" um Metadaten ergaenzen, damit rrze-glossary funktioniert:
     if ( get_option( 'rrze-glossary-metadata' ) != 'added' ) {
         addMetadata();
         update_option( 'rrze-glossary-metadata', 'added' );
