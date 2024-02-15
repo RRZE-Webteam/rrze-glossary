aeea2b84c05c047339777b5c01cf367404e8a9a1
Benjamin Klemencic <benjamin.klemencic@fau.de>
2024-02-15 16:59:17 +0100

    fixes warning "creation of dynamic property Settings::$optionsPage is deprecated"

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '1885f7f734d1584f7857');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '4d5693e43da12a592316');
